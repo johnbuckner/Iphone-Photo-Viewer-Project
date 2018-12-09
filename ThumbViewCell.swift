1cf2c27ca951c2c4a8cd52c7072deb21d064820f
John Buckner <45539069+johnbuckner@users.noreply.github.com>
2018-12-09 12:16:01 -0600

    ThumbViewCell.swift

---
+++ ThumbViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ThumbViewCell.swift
+//  Photo Viewer Project
+//
+//  Created by Student on 5/7/17.
+//  Copyright © 2017 John Buckner. All rights reserved.
+//
+
+import UIKit
+
+class ThumbViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImageView: UIImageView!
+}
